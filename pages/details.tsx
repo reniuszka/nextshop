5b58d24cc148e96e18d9d9e5d976f0899d99b93f
reniuszka <renia.diurczak@gmail.com>
2022-06-15 16:53:11 +0200

    lekcja 2 poprawione3

--- pages/details.tsx
+++ pages/details.tsx
@@ -121,10 +121,10 @@ export default function Example() {
                     {({ active }) => (
                       <a
                         href='#'
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
+                        // className={classNames(
+                        //   active ? 'bg-gray-100' : '',
+                        //   'block px-4 py-2 text-sm text-gray-700'
+                        // )}
                       >
                         Edit
                       </a>
@@ -134,10 +134,10 @@ export default function Example() {
                     {({ active }) => (
                       <a
                         href='#'
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
+                        // className={classNames(
+                        //   active ? 'bg-gray-100' : '',
+                        //   'block px-4 py-2 text-sm text-gray-700'
+                        // )}
                       >
                         View
                       </a>
