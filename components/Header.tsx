31a3b1c2d10da5474dc45d693fbb3723d227b425
reniuszka <renia.diurczak@gmail.com>
2022-06-15 16:30:07 +0200

    zadanie1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,23 @@
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 export const Header = () => {
+  const router = useRouter();
+
   return (
     <header className='max-w-md mx-auto w-full'>
-      <nav className='bg-gray-900 px-4 py-2 text-white'>
-        <Link href='/'>Główna</Link>
-        <Link href='/about'>About</Link>
+      <nav className='bg-gray-900 px-4 py-2 text-white flex gap-4'>
+        {/* <Link href='/'>Główna</Link> */}
+        <Link href='/'>
+          <a className={router.pathname == '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link href='/about'>
+          <a className={router.pathname == '/about' ? 'active' : ''}>About</a>
+        </Link>
+        <Link href='/details'>
+          <a className={router.pathname == '/details' ? 'active' : ''}>
+            Details
+          </a>
+        </Link>
       </nav>
     </header>
   );
